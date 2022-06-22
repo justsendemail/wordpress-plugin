9b76e12fbd18c6989e52892836043bd2cc482789
JustSend.Email <97758643+justsendemail@users.noreply.github.com>
2022-06-22 17:15:20 -0400

    clean up build script and detect missing version

--- build-zip.sh
+++ build-zip.sh
@@ -3,12 +3,25 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR;
 
+if [ "X$1" == "X" ]; then
+  echo "Usage: ./build-zip.sh <version>"
+  exit;
+fi
+
+# Build the React App in Production Mode
 cd settings-app;
 npm run dist
+# Copy the minified JavaScript to
+# the Plugin js/ directory
 cp dist/index_bundle.js* ../js/;
+
+# Pop back to the script dir
+cd $DIR;
+# And get its basename
+src_dir=$(basename $DIR)
 cd ..;
-cd ..;
-zip -r just-send-email-connect-$1.zip just-send-email-connect/ \
+
+zip -r just-send-email-connect-$1.zip $src_dir/ \
   -x '*/settings-app/node_modules/*' \
   -x '*/settings-app/*-lock*' \
   -x '*/settings-app/dist/*' \
